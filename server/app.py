72d95d2d559f057cfec2f2d99111dbb84c51cc02
Ernest <ernestwinata@gmail.com>
2023-10-10 08:50:34 -0700

    paste app

--- server/app.py
+++ server/app.py
@@ -1,33 +1,36 @@
-#!/usr/bin/env python3
-
-from flask import request, session
+from flask import request, session, jsonify
 from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
 
-from config import app, db, api
-from models import User, Recipe
+from config import db
+from models import User
 
 class Signup(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+        
+        username = data.get('username')
+        password = data.get('password')
+        image_url = data.get('image_url', '')
+        bio = data.get('bio', '')
 
-class CheckSession(Resource):
-    pass
+        
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            return {'error': 'Username already taken'}, 422
 
-class Login(Resource):
-    pass
+        new_user = User(username=username, image_url=image_url, bio=bio)
+        new_user.password = password
 
-class Logout(Resource):
-    pass
-
-class RecipeIndex(Resource):
-    pass
-
-api.add_resource(Signup, '/signup', endpoint='signup')
-api.add_resource(CheckSession, '/check_session', endpoint='check_session')
-api.add_resource(Login, '/login', endpoint='login')
-api.add_resource(Logout, '/logout', endpoint='logout')
-api.add_resource(RecipeIndex, '/recipes', endpoint='recipes')
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            session['user_id'] = new_user.id
+            return new_user.to_dict(), 201
+        except IntegrityError:
+            db.session.rollback()
+            return {'error': 'Could not create user'}, 422
 
 
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+
+
